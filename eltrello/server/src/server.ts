09d3ba827fdced15eaef7f4c17ef3b8299784abd
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2023-04-24 06:09:43 +0500

    Setting up Websocket in nodejs

--- eltrello/server/src/server.ts
+++ eltrello/server/src/server.ts
@@ -1,1 +1,28 @@
-console.log('Welcome to the server');
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import mongoose from 'mongoose';
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', () => {
+  console.log('Connected');
+});
+
+mongoose.connect('mongodb://localhost:27017/eltrello').then(() => {
+  console.log('Connected to MongoDB');
+  httpServer.listen(3000, () => {
+    console.log('Listening on port 3000');
+  });
+});
